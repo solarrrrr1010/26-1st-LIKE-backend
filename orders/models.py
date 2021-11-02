fd2e2f1f618b7562fc6be4578033f95469f58b3e
jieun <jieunlee.me@gmail.com>
2021-11-02 19:12:26 +0900

    Add: App 생성 및 models.py 작성

---
+++ orders/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+class ShoppingCart(models.Model):
+    product_option = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE)
+    user           = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    quantity       = models.IntegerField(default=0)
+    created_at     = models.DateTimeField(auto_now_add=True)
+    updated_at     = models.DateTimeField(auto_now=True)
+    deleted_at     = models.DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'shopping_carts'
+    
+class Order(models.Model):
+    project_option   = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE)
+    user             = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    order_number     = models.CharField(max_length=100)
+    status           = models.CharField(max_length=10)
+    shipping_address = models.CharField(max_length=2000)
+    created_at       = models.DateTimeField(auto_now_add=True)
+    updated_at       = models.DateTimeField(auto_now=True)
+    deleted_at       = models.DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'orders'
+
+class Review(models.Model):
+    project_option = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE)
+    user           = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    title          = models.CharField(max_length=100)
+    text           = models.CharField(max_length=2000)
+    rating         = models.DecimalField(max_digits=1, decimal_places=1)
+    created_at     = models.DateTimeField(auto_now_add=True)
+    updated_at     = models.DateTimeField(auto_now=True)
+    deleted_at     = models.DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'reviews'
+
+
+class ReviewImage(models.Model):
+    review = models.ForeignKey('Review', on_delete=models.CASCADE)
+    url    = models.CharField(max_length=2000)
+
+    class Meta:
+        db_table = 'review_images'
+
---
+++ products/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+class MainCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'main_categories'
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    main = models.ForeignKey('MainCategory', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'sub_categories'
+
+
+class Product(models.Model):
+    serial            = models.CharField(max_length=200)
+    title             = models.CharField(max_length=100)
+    sub_title         = models.CharField(max_length=100)
+    price             = models.DecimalField(max_digits=9, decimal_places=2)
+    thumbnail         = models.CharField(max_length=2000,blank=True)
+    eco_friendly      = models.BooleanField(default=False)
+    current_color     = models.CharField(max_length=2000)
+    description_title = models.CharField(max_length=2000)
+    description       = models.CharField(max_length=2000)
+
+    class Meta:
+        db_table = 'products'
+    
+class ProductOption(models.Model):
+    product  = models.ForeignKey('Product', on_delete=models.CASCADE)
+    color    = models.ForeignKey('Color', on_delete=models.CASCADE)
+    size     = models.ForeignKey('Size', on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'product_options'
+
+class ProductImage(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    url     = models.CharField(max_length=2000)
+
+    class Meta:
+        db_table = 'product_images'
+
+class Size(models.Model):
+    type = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'sizes'
+
+class Color(models.Model):
+    name = models.CharField(max_length=2000)
+    hex  = models.CharField(max_length=2000)
+
+    class Meta:
+        db_table = 'colors'
+
