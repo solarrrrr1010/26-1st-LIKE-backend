eea1e5561a94d61a38b62a35f5dbef83d2ef72ae
jieun <jieunlee.me@gmail.com>
2021-11-04 18:00:43 +0900

    Add: CategoryListView 구현

--- products/urls.py
+++ products/urls.py
@@ -1,3 +1,6 @@
+
+from django.urls    import path
+
 app_name = 'products'
 urlpatterns = [
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views       import View
+from django.http        import JsonResponse
+from products.models    import MainCategory, SubCategory 
 
-# Create your views here.
+class CategoryListView(View):
+    def get(self, request):
+        results = [
+            {
+                "id"   : main.id,
+                "name" : main.name,
+                "sub_categories" : [
+                    {
+                        "id"   : sub.id,
+                        "name" : sub.name,
+                    }
+                    for sub in SubCategory.objects.filter(main_category_id=main.id)
+                ]
+            }
+            for main in MainCategory.objects.all() 
+        ]
+        return JsonResponse({'results' : results}, status = 200)
