bc8243b2b25fd1c5863362b1c28a2a815ab6b070
jieun <jieunlee.me@gmail.com>
2021-11-04 19:20:02 +0900

    Modify: CategoryListView List comprehension intent 조정

--- products/views.py
+++ products/views.py
@@ -4,18 +4,12 @@ from products.models    import MainCategory, SubCategory
 
 class CategoryListView(View):
     def get(self, request):
-        results = [
-            {
-                "id"   : main.id,
-                "name" : main.name,
-                "sub_categories" : [
-                    {
-                        "id"   : sub.id,
-                        "name" : sub.name,
-                    }
-                    for sub in SubCategory.objects.filter(main_category_id=main.id)
-                ]
-            }
-            for main in MainCategory.objects.all() 
-        ]
+        results = [{
+            "id"   : main.id,
+            "name" : main.name,
+            "sub_categories" : [{
+                "id"   : sub.id,
+                "name" : sub.name,
+            } for sub in SubCategory.objects.filter(main_category_id=main.id)]
+        } for main in MainCategory.objects.all() ]
         return JsonResponse({'results' : results}, status = 200)
