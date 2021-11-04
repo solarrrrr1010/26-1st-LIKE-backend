02de6d443d12a78634252187ed484c779af15604
kimbongcheol <solarrrrr1010@gmail.com>
2021-11-04 19:42:29 +0900

    modify: 리뷰 반영 수정

--- users/views.py
+++ users/views.py
@@ -8,36 +8,40 @@ from .models import User
 
 class SignupView(View):
     def post(self, request):
-        data         = json.loads(request.body)
-        name         = data["name"]
-        email        = data["email"]
-        password     = data["password"]
-        phone_number = data["phone_number"]
-        address      = data["address"]
+        try:
+            data         = json.loads(request.body)
+            name         = data["name"]
+            email        = data["email"]
+            password     = data["password"]
+            phone_number = data["phone_number"]
+            address      = data["address"]
 
-        if not (email and password):
-            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
-    
-        if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-_]+$', email):
-            return JsonResponse({"message" : "EMAIL_VALIDATION_ERROR"}, status=400)
+            if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-_]+$', email):
+                return JsonResponse({"message" : "EMAIL_VALIDATION_ERROR"}, status=400)
 
-        if not re.match('^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~₩!@#$%^&*()\-_=+])[a-zA-Z0-9~!@#$%^&*()_\-+=]{8,}$', password):
-            return JsonResponse({"message" : "PASSWORD_VALIDATION_ERROR"}, status=400)
-        
-        if User.objects.filter(email=email).exists():
-            return JsonResponse({"message" : "DUPLICATION_ERROR"}, status=400)
+            if not re.match('^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~₩!@#$%^&*()\-_=+])[a-zA-Z0-9~!@#$%^&*()_\-+=]{8,}$', password):
+                return JsonResponse({"message" : "PASSWORD_VALIDATION_ERROR"}, status=400)
+            
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({"message" : "DUPLICATION_ERROR"}, status=400)
 
-        hashed_password = bcrypt.hashpw(data["password"].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-        User.objects.create(
-            email        = email, 
-            password     = hashed_password,
-            name         = name,
-            phone_number = phone_number,
-            address      = address
+            User.objects.create(
+                email        = email, 
+                password     = hashed_password,
+                name         = name,
+                phone_number = phone_number,
+                address      = address
             )
-        
-        return JsonResponse({"message" : "SUCCESS"}, status=201)
+            
+            return JsonResponse({"message" : "SUCCESS"}, status=201)
+
+        except User.DoesNotExist:
+            return JsonResponse({"message" : "USER_DOES_NOT_EXISTS"}, status=401) 
+
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=401)     
 
 class LoginView(View):
     def post(self, request):
@@ -48,13 +52,13 @@ class LoginView(View):
             user = User.objects.get(email=email)
             
             if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                return JsonResponse({"MESSAGE" : "INVALID_USER"}, status=401)
+                return JsonResponse({"message" : "INVALID_USER"}, status=401)
 
             access_token = jwt.encode({"id" : user.id}, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
-            return JsonResponse({"MESSAGE" : "SUCCESS", "ACCESS_TOKEN" : access_token}, status=200)
+            return JsonResponse({"message" : "SUCCESS", "ACCESS_TOKEN" : access_token}, status=200)
                 
         except User.DoesNotExist:
-            return JsonResponse({"message" : "USER_DOES_EXISTS"}, status=401) 
+            return JsonResponse({"message" : "USER_DOES_NOT_EXISTS"}, status=401) 
 
         except KeyError:
             return JsonResponse({"message" : "KEY_ERROR"}, status=401)               
