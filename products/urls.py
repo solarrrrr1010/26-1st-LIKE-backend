9cb5e3fb3f439a886ef0becb07d09aeefdcf8fc9
jieun <jieunlee.me@gmail.com>
2021-11-04 22:32:24 +0900

    Add: ProductListView 구현 - 메인페이지의 전체 상품 리스트 조회 구현

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,13 @@
 from django.urls    import path
+from products.views import (
+    ProductListView, 
+    # ProductDetailView, 
+)
 
 app_name = 'products'
 urlpatterns = [
+    path('', ProductListView.as_view()),
+    # path('/<int:product_id>', ProductDetailView.as_view()),
+    # path('/<int:main_id>' , ProductTypeView.as_view()),
+    # path('/<int:main_id>/<int:sub_id>' , ProductView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.views       import View
 from django.http        import JsonResponse
-from products.models    import MainCategory, SubCategory 
+from products.models    import MainCategory, SubCategory, Product 
 
 class CategoryListView(View):
     def get(self, request):
@@ -12,4 +12,30 @@ class CategoryListView(View):
                 "name" : sub.name,
             } for sub in SubCategory.objects.filter(main_category_id=main.id)]
         } for main in MainCategory.objects.all() ]
+        return JsonResponse({'results' : results}, status = 200)
+
+class ProductListView(View):
+    def get(self, request):
+        results = [{
+            "id"                  : product.id,
+            "serial"              : product.serial,
+            "title"               : product.title,
+            "sub_title"           : product.sub_title,
+            "price"               : product.price,
+            "thumbnail_image_url" : product.thumbnail_image_url,
+            "eco_friendly"        : product.eco_friendly
+        } for product in Product.objects.all()]
+        return JsonResponse({'results' : results}, status = 200)
+
+class ShoesListView(View):
+    def get(self, request, category):
+        results = [{
+            "id"                  : product.id,
+            "serial"              : product.serial,
+            "title"               : product.title,
+            "sub_title"           : product.sub_title,
+            "price"               : product.price,
+            "thumbnail_image_url" : product.thumbnail_image_url,
+            "eco_friendly"        : product.eco_friendly
+        } for product in Product.objects.filter(sub_=category)]
         return JsonResponse({'results' : results}, status = 200)
