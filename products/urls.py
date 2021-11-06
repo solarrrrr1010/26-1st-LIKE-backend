645a336c043f4e796b814972569e9eea00ee1f82
jieun <jieunlee.me@gmail.com>
2021-11-06 17:00:00 +0900

    Add: Sub category별 상품 리스트 조회 추가 - ProductGroupListView 에 로직 구현

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls    import path
 from products.views import (
     ProductListView, 
+    ProductGroupListView, 
 )
 
 app_name = 'products'
 urlpatterns = [
     path('', ProductListView.as_view()),
+    path('/<int:sub_category_id>', ProductGroupListView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -17,20 +17,20 @@ class CategoryListView(View):
 
 class ProductListView(View):
     def get(self, request):
-        category = request.GET.get('category', None)
-        color    = request.GET.get('color', None)
-        size     = request.GET.get('size', None)
-        sort     = request.GET.get('sort', '-id')
+        main_category = request.GET.get('category', None)
+        colors        = request.GET.getlist('color')
+        sizes         = request.GET.getlist('size')
+        sort          = request.GET.get('sort', '-id')
         
         condition = Q()
-        ordering = f'product__{sort}'
+        ordering  = f'product__{sort}'
 
-        if category:
-            condition.add(Q(product__sub_category__main_category__name=category), Q.AND)
-        if color:
-            condition.add(Q(color__name=color), Q.AND)
-        if size:
-            condition.add(Q(size__name=category), Q.AND)
+        if main_category:
+            condition &= Q(product__sub_category__main_category__name=main_category) 
+        if colors:
+            [condition.add(Q(color__name=color), Q.OR) for color in colors]
+        if sizes:
+            [condition.add(Q(size__type=size), Q.OR) for size in sizes]
 
         if '-' in sort:
             ordering = f"-{ordering.replace('-','')}" 
@@ -45,6 +45,42 @@ class ProductListView(View):
             "price"               : po.product.price,
             "thumbnail_image_url" : po.product.thumbnail_image_url,
             "eco_friendly"        : po.product.eco_friendly,
+            "sub_category"        : po.product.sub_category.name,  
+            "main_category"       : po.product.sub_category.main_category.name
+        } for po in ProductOption.objects.filter(condition).order_by(ordering)]
+
+        return JsonResponse({'results' : results}, status = 200)
+
+class ProductGroupListView(View):
+    def get(self, request, sub_category_id):
+        colors = request.GET.getlist('color')
+        sizes  = request.GET.getlist('size')
+        sort   = request.GET.get('sort', '-id')
+        
+        condition = Q()
+        ordering = f'product__{sort}'
+
+        if sub_category_id:
+            condition &= Q(product__sub_category__id=sub_category_id) 
+        if colors:
+            [condition.add(Q(color__name=color), Q.OR) for color in colors]
+        if sizes:
+            [condition.add(Q(size__type=size), Q.OR) for size in sizes]
+
+        if '-' in sort:
+            ordering = f"-{ordering.replace('-','')}" 
+
+        results = [{
+            "product"             : po.product_id,
+            "color"               : po.color.name,
+            "size"                : po.size.type,
+            "serial"              : po.product.serial,
+            "title"               : po.product.title,
+            "sub_title"           : po.product.sub_title,
+            "price"               : po.product.price,
+            "thumbnail_image_url" : po.product.thumbnail_image_url,
+            "eco_friendly"        : po.product.eco_friendly,
+            "sub_category"        : po.product.sub_category.name,  
             "main_category"       : po.product.sub_category.main_category.name
         } for po in ProductOption.objects.filter(condition).order_by(ordering)]
 
