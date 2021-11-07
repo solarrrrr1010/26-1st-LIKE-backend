1223547e647bbd7163a7727bc006783ff7381505
jieun <jieunlee.me@gmail.com>
2021-11-07 21:27:56 +0900

    Modify: ProductListView ORM수정 - ProductGroupListView 삭제 - ProductListView 조회하는 테이블 변경(ProductOption -> Product)

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,7 @@
 from django.urls    import path
-from products.views import ProductListView, ProductGroupListView 
+from products.views import ProductListView 
 
 app_name = 'products'
 urlpatterns = [
     path('', ProductListView.as_view()),
-    path('/<int:sub_category_id>', ProductGroupListView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.views       import View
 from django.http        import JsonResponse
-from django.db.models   import Q
-from products.models    import MainCategory, SubCategory, ProductOption
+from django.db.models   import Sum
+
+from products.models    import MainCategory, SubCategory, Product
 
 class CategoryListView(View):
     def get(self, request):
@@ -18,62 +19,33 @@ class CategoryListView(View):
 class ProductListView(View):
     def get(self, request):
         filter_field = {
-            'category' : "product__sub_category__main_category__name__in",  
-            'color'    : "color__name__in",
-            'size'     : "size__type__in",
+            'main_category' : "sub_category__main_category__id__in",  
+            'sub_category'  : "sub_category__id__in",
+            'color'         : "productoption__color__name__in",
+            'size'          : "productoption__size__type__in",
         }
         filter_set = {
             filter_field.get(key) : value for (key, value) in dict(request.GET).items() if filter_field.get(key)
         }
 
         sort     = request.GET.get('sort', '-id')
-        ordering = f'product__{sort}'
+        ordering = f'{sort}'
         if '-' in sort:
             ordering = f"-{ordering.replace('-','')}" 
-
+        
         results = [{
-            "product"             : po.product_id,
-            "color"               : po.color.name,
-            "size"                : po.size.type,
-            "serial"              : po.product.serial,
-            "title"               : po.product.title,
-            "sub_title"           : po.product.sub_title,
-            "price"               : po.product.price,
-            "thumbnail_image_url" : po.product.thumbnail_image_url,
-            "eco_friendly"        : po.product.eco_friendly,
-            "sub_category"        : po.product.sub_category.name,  
-            "main_category"       : po.product.sub_category.main_category.name
-        } for po in ProductOption.objects.filter(**filter_set).order_by(ordering)]
-
-        return JsonResponse({'results' : results}, status = 200)
-
-class ProductGroupListView(View):
-    def get(self, request, sub_category_id):
-        filter_field = {
-            'color'    : "color__name__in",
-            'size'     : "size__type__in",
-        }
-        filter_set = {
-            filter_field.get(key) : value for (key, value) in dict(request.GET).items() if filter_field.get(key)
-        }
-
-        sort     = request.GET.get('sort', '-id')
-        ordering = f'product__{sort}'
-        if '-' in sort:
-            ordering = f"-{ordering.replace('-','')}" 
-
-        results = [{
-            "product"             : po.product_id,
-            "color"               : po.color.name,
-            "size"                : po.size.type,
-            "serial"              : po.product.serial,
-            "title"               : po.product.title,
-            "sub_title"           : po.product.sub_title,
-            "price"               : po.product.price,
-            "thumbnail_image_url" : po.product.thumbnail_image_url,
-            "eco_friendly"        : po.product.eco_friendly,
-            "sub_category"        : po.product.sub_category.name,  
-            "main_category"       : po.product.sub_category.main_category.name
-        } for po in ProductOption.objects.filter(product__sub_category__id=sub_category_id).filter(**filter_set).order_by(ordering)]
+            "product"             : product.id,
+            "serial"              : product.serial,
+            "title"               : product.title,
+            "sub_title"           : product.sub_title,
+            "price"               : product.price,
+            "thumbnail_image_url" : product.thumbnail_image_url,
+            "eco_friendly"        : product.eco_friendly,
+            "color"               : product.productoption_set.first().color.name,
+            "size"                : [po.size.type for po in product.productoption_set.all()],
+            "quantity"            : product.productoption_set.values('quantity').aggregate(Sum('quantity'))['quantity__sum'],
+            "sub_category"        : product.sub_category.name,  
+            "main_category"       : product.sub_category.main_category.name
+        } for product in Product.objects.filter(**filter_set).order_by(ordering)]
 
         return JsonResponse({'results' : results}, status = 200)
