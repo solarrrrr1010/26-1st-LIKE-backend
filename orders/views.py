8d57080cc5d7831d29f7a890cb29c684c824fdb1
jieun <jieunlee.me@gmail.com>
2021-11-08 18:35:49 +0900

    Modify: OrderListView GET method 구현

--- orders/views.py
+++ orders/views.py
@@ -3,10 +3,33 @@ from json
 from django.views import View
 from django.http  import JsonResponse
 
+from orders.models import Order
+
 class OrderListView(View):
+    # @login_required
     def get(self, request):
-        return JsonResponse({"results" : "results"}, status=200)
+        results = [{
+            "id"                  : order.id,
+            "order_number"        : order.order_number,
+            "status"              : order.order_status.name,
+            "shipping_address"    : order.shipping_address,
+            "product_id"          : order.product_option.product.id,
+            "user_id"             : order.user_id,
+            "product_title"       : order.product_option.product.title,
+            "serial"              : order.product_option.product.serial,
+            "size"                : order.product_option.size.type,
+            "quantity"            : order.quantity,
+            "price"               : order.price,
+            "thumbnail_image_url" : order.product_option.product.thumbnail_image_url,
+        } for order in Order.objects.filter(user_id=request.user.id)]
+        return JsonResponse({"results" : results}, status=200)
         
+    # @login_required
     def post(self, request):
-        return JsonResponse({"message" : "SUCCESS"}, status=201)
+        try:
+
+            return JsonResponse({"message" : "SUCCESS"}, status=201)
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+
         
