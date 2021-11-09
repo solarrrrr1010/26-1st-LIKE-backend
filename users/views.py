879c79e1393b6b5bc4e7605d5a6087c1084de7fb
kimbongcheol <solarrrrr1010@gmail.com>
2021-11-09 18:01:41 +0900

    modify: 회원가입 전화번호 하이픈 없이 숫자만 받기, 이메일과 비밀번호 에러 메시지 특정 못 하게 반환

--- users/views.py
+++ users/views.py
@@ -21,6 +21,9 @@ class SignupView(View):
             if not re.match('^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~₩!@#$%^&*()\-_=+])[a-zA-Z0-9~!@#$%^&*()_\-+=]{8,}$', password):
                 return JsonResponse({"message" : "PASSWORD_VALIDATION_ERROR"}, status=400)
             
+            if not re.match('^(?=.*[0-9]{10,11}$', phone_number):
+                return JsonResponse({"message" : "PHONE_NUMBER_VALIDATION_ERROR"}, status=400)
+            
             if User.objects.filter(email=email).exists():
                 return JsonResponse({"message" : "DUPLICATION_ERROR"}, status=400)
 
