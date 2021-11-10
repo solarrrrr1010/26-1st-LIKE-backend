c7361cce52ea3f0d1be58d7894e72beff709bcb8
kimbongcheol <solarrrrr1010@gmail.com>
2021-11-10 19:40:23 +0900

    add: 리뷰 리스트 GET 구현-fix

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,9 @@
 from django.urls    import path
 
-from orders.views   import OrderListView
+from orders.views   import OrderListView, ReviewView
 
 app_name = 'orders'
 urlpatterns = [
     path('', OrderListView.as_view()),
-    path('/details/<int:product_id>')
+    path('/details/<int:product_id>', ReviewView.as_view())
 ]
