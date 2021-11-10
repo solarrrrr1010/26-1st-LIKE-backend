afe1c6c3f3fdb18cd9a7fce49e2361d8fb7abda3
jieun <jieunlee.me@gmail.com>
2021-11-11 00:10:53 +0900

    Add: Redis cache 테스트, 시간측정 decorator 생성

--- like/settings.py
+++ like/settings.py
@@ -155,14 +155,12 @@ CORS_ALLOW_HEADERS = (
 )
 
 # redis
-CACHE_TTL = 30
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
         'LOCATION': 'redis://127.0.0.1:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            'TIMEOUT': 5,
         },
     }
 }
